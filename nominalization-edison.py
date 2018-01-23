a65138662e3a86eb9b1f9b8f73842bbcb0b8a42c
Brian Croxall <briancroxall@users.noreply.github.com>
2018-01-23 09:04:52 -0700

    clean code to meet PEP8 guidelines

--- nominalization-edison.py
+++ nominalization-edison.py
@@ -1,27 +1,43 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Mon Jan 22 13:57:43 2018
-
-@author: briancroxall
+Assignment:
+    Use regular expressions and lists to correctly identify and
+    count all of the nominalizations in a short text.
+    You can simply paste a short text into a variable in your script.
 """
+import re
 
 '''
 TEXT:
 
-Thomas Alva Edison (February 11, 1847 – October 18, 1931) was an American inventor and businessman, who has been described as America's greatest inventor. He developed many devices that greatly influenced life around the world, including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb. Dubbed "The Wizard of Menlo Park", he was one of the first inventors to apply the principles of mass production and large-scale teamwork to the process of invention, and because of that, he is often credited with the creation of the first industrial research laboratory.
+Thomas Alva Edison (February 11, 1847 – October 18, 1931) was an American
+inventor and businessman, who has been described as America's greatest
+inventor. He developed many devices that greatly influenced life around the
+world, including the phonograph, the motion picture camera, and the
+long-lasting, practical electric light bulb. Dubbed "The Wizard of Menlo
+Park", he was one of the first inventors to apply the principles of mass
+production and large-scale teamwork to the process of invention, and because
+of that, he is often credited with the creation of the first industrial
+research laboratory.
 
-Edison was a prolific inventor, holding 1,093 US patents in his name, as well as many patents in the United Kingdom, France, and Germany. More significant than the number of Edison's patents was the widespread impact of his inventions: electric light and power utilities, sound recording, and motion pictures all established major new industries worldwide. Edison's inventions contributed to mass communication and, in particular, telecommunications. These included a stock ticker, a mechanical vote recorder, a battery for an electric car, electrical power, recorded music and motion pictures. His advanced work in these fields was an outgrowth of his early career as a telegraph operator. Edison developed a system of electric-power generation and distribution to homes, businesses, and factories – a crucial development in the modern industrialized world. His first power station was on Pearl Street in Manhattan, New York.
+Edison was a prolific inventor, holding 1,093 US patents in his name, as well
+as many patents in the United Kingdom, France, and Germany. More significant
+than the number of Edison's patents was the widespread impact of his
+inventions: electric light and power utilities, sound recording, and motion
+pictures all established major new industries worldwide. Edison's inventions
+contributed to mass communication and, in particular, telecommunications.
+These included a stock ticker, a mechanical vote recorder, a battery for an
+electric car, electrical power, recorded music and motion pictures. His
+advanced work in these fields was an outgrowth of his early career as a
+telegraph operator. Edison developed a system of electric-power generation
+and distribution to homes, businesses, and factories – a crucial development
+in the modern industrialized world. His first power station was on Pearl
+Street in Manhattan, New York.
 
 '''
 
-'''
-Assignment:
-    Use regular expressions and lists to correctly identify and count all of the nominalizations in a short text.
-    You can simply paste a short text into a variable in your script.
-'''
-import re
-words = 'Thomas Alva Edison (February 11, 1847 – October 18, 1931) was an American inventor and businessman, who has been described as America\'s greatest inventor. He developed many devices that greatly influenced life around the world, including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb. Dubbed "The Wizard of Menlo Park", he was one of the first inventors to apply the principles of mass production and large-scale teamwork to the process of invention, and because of that, he is often credited with the creation of the first industrial research laboratory. Edison was a prolific inventor, holding 1,093 US patents in his name, as well as many patents in the United Kingdom, France, and Germany. More significant than the number of Edison\'s patents was the widespread impact of his inventions: electric light and power utilities, sound recording, and motion pictures all established major new industries worldwide. Edison\'s inventions contributed to mass communication and, in particular, telecommunications. These included a stock ticker, a mechanical vote recorder, a battery for an electric car, electrical power, recorded music and motion pictures. His advanced work in these fields was an outgrowth of his early career as a telegraph operator. Edison developed a system of electric-power generation and distribution to homes, businesses, and factories – a crucial development in the modern industrialized world. His first power station was on Pearl Street in Manhattan, New York.'
+words = 'Thomas Alva Edison (February 11, 1847 – October 18, 1931) was an American inventor and businessman, who has been described as America\'s greatest inventor. He developed many devices that greatly influenced life around the world, including the phonograph, the motion picture camera, and the long-lasting, practical electric light bulb. Dubbed "The Wizard of Menlo Park", he was one of the first inventors to apply the principles of mass production and large-scale teamwork to the process of invention, and because of that, he is often credited with the creation of the first industrial research laboratory. Edison was a prolific inventor, holding 1,093 US patents in his name, as well as many patents in the United Kingdom, France, and Germany. More significant than the number of Edison\'s patents was the widespread impact of his inventions: electric light and power utilities, sound recording, and motion pictures all established major new industries worldwide. Edison\'s inventions contributed to mass communication and, in particular, telecommunications. These included a stock ticker, a mechanical vote recorder, a battery for an electric car, electrical power, recorded music and motion pictures. His advanced work in these fields was an outgrowth of his early career as a telegraph operator. Edison developed a system of electric-power generation and distribution to homes, businesses, and factories – a crucial development in the modern industrialized world. His first power station was on Pearl Street in Manhattan, New York.'  # noqa: E501
 print('text:', words)
 print()  # blank line for readability
 
@@ -54,7 +70,7 @@ other_re = r'(\w{2,}(?:tion|sion|ment|ence|ance)s?)\b'
 print('regex other nominalized verbs formed with suffixes:', other_re)
 result_other = re.findall(other_re, words, re.I)
 print('re.findall():', result_other)
-print('Number of other nominalized verbs formed with suffixes:', len(result_other))
+print('Number of other nominalized verbs formed with suffixes:', len(result_other))  # noqa: E501
 print()
 
 # Zero-change nominalization
@@ -66,5 +82,5 @@ print('Number of zero-change nominalizations:', len(result_zero))
 print()
 
 # Conclusion
-total = result_gerund + result_agent + result_recipient + result_other + result_zero
+total = result_gerund + result_agent + result_recipient + result_other + result_zero  # noqa: E501
 print('Total number of nominalizations:', len(total))
