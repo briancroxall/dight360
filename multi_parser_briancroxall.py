57eab384ff8c168d6253f2bb4e048139e2937a6a
Brian Croxall <briancroxall@users.noreply.github.com>
2018-01-31 14:01:21 -0700

    Indent to correctly nest loops

--- multi_parser_briancroxall.py
+++ multi_parser_briancroxall.py
@@ -22,38 +22,31 @@ for filename in glob('*.html'):
         text = my_file.read()
 
 # Find mascots
-mascots_re = r'Nickname</a></th>\n<td>(.*?)</td>' #regex query
-print('regex for mascots:', mascots_re) #display the regex string in the console #noqa: E501
-result_mascot = re.findall(mascots_re, text, re.I) #run query with findall
-with open(filename + '.txt', 'w') as my_file1:
-    print('Mascot: ', result_mascot, file = my_file1) #save results to new file
-print('re.findall():', result_mascot) # print results to console
-print()
+    mascots_re = r'Nickname</a></th>\n<td>(.*?)</td>' #regex query
+    print('regex for mascots:', mascots_re) #display the regex string in the console #noqa: E501
+    result_mascot = re.findall(mascots_re, text, re.I) #run query with findall
+    with open(filename + '.txt', 'w') as my_file1:
+        print('Mascot: ', result_mascot, file = my_file1) #save results to new file
+    print('re.findall():', result_mascot) # print results to console
+    print()
 
 #Find population 
-population_re = r'Enrollment</th>\n<td>(.*?)<'
-print('regex for population:', population_re)
-result_population = re.findall(population_re, text, re.I)
-with open(filename + '.txt', 'a') as my_file2:
-    print('School population:', result_population, file = my_file2)
-print('re.findall():', result_population)
-print()
+    population_re = r'Enrollment</th>\n<td>(.*?)<'
+    print('regex for population:', population_re)
+    result_population = re.findall(population_re, text, re.I)
+    with open(filename + '.txt', 'a') as my_file2:
+        print('School population:', result_population, file = my_file2)
+    print('re.findall():', result_population)
+    print()
 
 #Find coordinates
-geo_re = r'span class="geo">(.*?)</span'
-print('regex for coordinates:', geo_re)
-result_geo = re.findall(geo_re, text, re.I)
-with open(filename + '.txt', 'a') as my_file3:
-    print('Coordinates:', result_geo, file = my_file3)
-print('re.findall():', result_geo)
-print()
+    geo_re = r'span class="geo">(.*?)</span'
+    print('regex for coordinates:', geo_re)
+    result_geo = re.findall(geo_re, text, re.I)[0] #added zero-eth index to get only instance
+    with open(filename + '.txt', 'a') as my_file3:
+        print('Coordinates:', result_geo, file = my_file3)
+    print('re.findall():', result_geo)
+    print()
 
 #Show name of outputted file
 print('File saved as', filename)
-
-"""
-from glob import glob
-
-for filename in glob('*.py'):
-    print(filename)
-"""
