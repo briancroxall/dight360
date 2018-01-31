efe40be0555c7386c569b7fa3593d15d59416f79
Brian Croxall <briancroxall@users.noreply.github.com>
2018-01-31 14:24:33 -0700

    Correct for PEP8 formatting

--- multi_parser_briancroxall.py
+++ multi_parser_briancroxall.py
@@ -10,11 +10,11 @@ Created on Tue Jan 30 18:28:10 2018
 @author: briancroxall
 
 Code to open file, parse with RegEx to find mascot name, school population
-size, and geo coordinates, and output to new file. 
+size, and geo coordinates, and output to new file.
 Attempting to run on multiple files.
 """
-from glob import glob
-import re #regular expressions 
+from glob import glob  # needed for bunching files
+import re  # regular expressions
 
 # open and read multiple files
 for filename in glob('*.html'):
@@ -22,32 +22,32 @@ for filename in glob('*.html'):
         text = my_file.read()
 
 # Find mascots
-    mascots_re = r'Nickname</a></th>\n<td>(.*?)</td>|Mascot</th>\n.*>(.*?)</a>|Mascot</th>\n<td>(.*?)</' #regex query
-    print('regex for mascots:', mascots_re) #display the regex string in the console #noqa: E501
-    result_mascot = re.findall(mascots_re, text, re.I) #run query with findall
+    mascots_re = r'Nickname</a></th>\n<td>(.*?)</td>|Mascot</th>\n.*>(.*?)</a>|Mascot</th>\n<td>(.*?)</'  # noqa: E501 regex query
+    print('regex for mascots:', mascots_re)  # noqa: E501 display the regex string in the console
+    result_mascot = re.findall(mascots_re, text, re.I)  # noqa: E501 run query with findall
     with open(filename + '.txt', 'w') as my_file1:
-        print('Mascot: ', result_mascot, file = my_file1) #save results to new file
-    print('re.findall():', result_mascot) # print results to console
+        print('Mascot: ', result_mascot, file=my_file1)  # noqa: E501 save results to new file
+    print('re.findall():', result_mascot)  # print results to console
     print()
 
-#Find population 
+# Find population
     population_re = r'Enrollment</th>\n<td>(.*?)<'
     print('regex for population:', population_re)
     result_population = re.findall(population_re, text, re.I)
     with open(filename + '.txt', 'a') as my_file2:
-        print('School population:', result_population, file = my_file2)
+        print('School population:', result_population, file=my_file2)
     print('re.findall():', result_population)
     print()
 
-#Find coordinates
+# Find coordinates
     geo_re = r'span class="geo">(.*?)</span'
     print('regex for coordinates:', geo_re)
-    result_geo = re.findall(geo_re, text, re.I)[0] #added zero-eth index to get only instance
+    result_geo = re.findall(geo_re, text, re.I)[0]  # noqa: E501 added zero-eth index to get only instance
     with open(filename + '.txt', 'a') as my_file3:
-        print('Coordinates:', result_geo, file = my_file3)
+        print('Coordinates:', result_geo, file=my_file3)
     print('re.findall():', result_geo)
     print()
 
-#Show name of outputted file
+# Show name of outputted file
     print('File saved as', filename)
     print()
