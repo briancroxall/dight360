bd0893dca13e58b17cf279e1d71b15e46b5a7b3e
Brian Croxall <briancroxall@users.noreply.github.com>
2018-01-30 18:29:26 -0700

    Initial commit

---
+++ parser_briancroxall.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Assignment: In this script, open the file and pseudo-parse it with a regular
+expression to extract the information you are interested in. Save that
+information in another file (<filename>_parsed.txt).
+
+Created on Tue Jan 30 18:28:10 2018
+
+@author: briancroxall
+
+Code to open file, parse with RegEx, and output to new file.
+"""
+import re
+
+
