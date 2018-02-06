c2e8ac97a2c3afb1da08b84389a1cc7b30d280c2
Brian Croxall <briancroxall@users.noreply.github.com>
2018-02-06 16:28:17 -0700

    Scrape one page and parse whole page

--- soup_scrape_briancroxall.py
+++ soup_scrape_briancroxall.py
@@ -5,7 +5,7 @@ Created on Mon Feb  5 16:11:28 2018
 
 @author: briancroxall
 
-
+Assignment: 
 Write a function called `get_rnlp` that requests an arbitrary filename
 (filename is an argument of the function) from
 `http://reynoldsnlp.com/scrape/` and saves the file in the local folder
@@ -25,3 +25,43 @@ same thing with all of those hrefs. Begin with 'aa.html'.
 """
 from bs4 import BeautifulSoup
 import re
+import random
+import requests as r
+import time
+
+headers1 = {'user-agent': 'Brian Croxall (brian.croxall@byu.edu)'}  # noqa: E501 identify self
+reynolds = 'http://reynoldsnlp.com/scrape/aa.html'  
+
+"""
+for page in reynolds:  
+    response = r.get(wikipedia + term, headers=headers1)
+    time.sleep(random.uniform(1.5, 2.5))  # give the server some rest
+    with open(term + '.html', 'w') as my_file:  # noqa: E501 open file in write mode, using name of query
+        print(response.text, file=my_file)
+"""
+# Scrape one page
+response = r.get(reynolds, headers=headers1)
+time.sleep(random.uniform(1.5, 2.5))
+with open('scrape/aa' + '.html', 'w') as my_file:
+    print(response.text, file=my_file)
+print('Success!')
+print()  
+    
+# Parse one file
+with open('scrape/aa.html') as aa_file:
+    soup = BeautifulSoup(aa_file, 'html.parser')
+print(soup.prettify())
+"""
+def get_rnlp    
+    
+def get_hrefs:
+    
+
+    
+    list comprehensions
+sent1 = 'This is an example sentence.'.split()
+[word for word in sent1 if 'a' in word]
+
+    
+for
+""" 
