7ace93f1d176647de61f6379422fe9b066c07a74
Brian Croxall <briancroxall@users.noreply.github.com>
2018-02-07 14:52:54 -0700

    PEP8 style changes

--- soup_scrape_briancroxall.py
+++ soup_scrape_briancroxall.py
@@ -5,7 +5,7 @@ Created on Mon Feb  5 16:11:28 2018
 
 @author: briancroxall
 
-Assignment: 
+Assignment:
 Write a function called `get_rnlp` that requests an arbitrary filename
 (filename is an argument of the function) from
 `http://reynoldsnlp.com/scrape/` and saves the file in the local folder
@@ -27,25 +27,27 @@ from bs4 import BeautifulSoup
 import random
 import requests as r
 import time
- 
+
 def get_rnlp(seed):
+
     """ This grabs a specified URL from Rob's site and saves it to a folder"""
     reynolds = 'http://reynoldsnlp.com/scrape/'  # stem for site
     headers1 = {'user-agent': 'Brian Croxall (brian.croxall@byu.edu)'}  # noqa: E501 identify self
-    response = r.get(reynolds + seed, headers=headers1)  
+    response = r.get(reynolds + seed, headers=headers1)
     time.sleep(random.uniform(1.5, 2.5))  # don't kill the server
     with open('scrape/' + seed, 'w') as my_file:  # open file
         print(response.text, file=my_file)  # write content of response to file
         print('Successfully scraped ' + seed)  # noqa: E501; prints a message so we know it worked
     print()  # blank line
-    
+
 def get_hrefs(filename):
-    """ Opens the file passed to it and parses for the other links on the page """  #noqa: E501
+
+    # Opens the file passed to it and parses for the other links on the page
     sites = []  # creates an empty list to add the URLs to
-    with open('scrape/' + filename) as html_file:  # opens the file passed to it
+    with open('scrape/' + filename) as html_file:  # noqa: E501 opens the file passed to it
         soup = BeautifulSoup(html_file, 'html.parser')  # noqa: E501 creates variable soup that has the read content of the file via BS
     for link in soup.find_all('a'):  # finds all the a elements on the page
-        # print('link type', type(link))  # prints the type of all the a elements
+        # print('link type', type(link))  # noqa: E501 prints the type of all the a elements
         url = link.get('href')  # gets the value for all the href keys
         end = url.split('/')[-1]  # splits the value (a url) on its last slash
         sites.append(end)  # adds the end of the url to the empty list
@@ -57,8 +59,8 @@ while len(site_list) > 0:
     item = site_list.pop()
     if item in done_list:
         continue
-    else: 
+    else:
         get_rnlp(item)  # runs first function on seed item
         sites = get_hrefs(item)  # runs second function on seed item
-        site_list.extend(sites)  # adds the list from the second function to the initial seed list
+        site_list.extend(sites)  # noqa: E501 adds the list from the second function to the initial seed list
         done_list.append(item)
