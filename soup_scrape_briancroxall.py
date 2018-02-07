d56ca96aaa8e8685d68eaabaa94b2ca02bce028f
Brian Croxall <briancroxall@users.noreply.github.com>
2018-02-06 21:59:18 -0700

    Abstract the parse function

--- soup_scrape_briancroxall.py
+++ soup_scrape_briancroxall.py
@@ -28,19 +28,7 @@ import random
 import requests as r
 import time
 
-headers1 = {'user-agent': 'Brian Croxall (brian.croxall@byu.edu)'}  # noqa: E501 identify self
-reynolds = 'http://reynoldsnlp.com/scrape/'  
 """
-Code for a single site implementation
-
-# Scrape one page
-response = r.get(reynolds, headers=headers1)
-time.sleep(random.uniform(1.5, 2.5))
-with open('scrape/aa' + '.html', 'w') as my_file:
-    print(response.text, file=my_file)
-print('Success!')
-print()  
-    
 # Parse one file
 sites = []
 with open('scrape/aa.html') as aa_file:
@@ -53,12 +41,11 @@ for link in soup.find_all('a'):
     sites.append(end)
 print(sites)
 """
-
-# print(soup.find_all(re.compile(r'([a-z][a-z].html)')))
  
 def get_rnlp(seed):
     """ This grabs a specified URL from Rob's site and saves it to a folder"""
     reynolds = 'http://reynoldsnlp.com/scrape/'  # stem for site
+    headers1 = {'user-agent': 'Brian Croxall (brian.croxall@byu.edu)'}  # noqa: E501 identify self
     response = r.get(reynolds + seed, headers=headers1)  
     time.sleep(random.uniform(1.5, 2.5))  # don't kill the server
     with open('scrape/' + seed, 'w') as my_file:  # open file
@@ -66,15 +53,16 @@ def get_rnlp(seed):
         print('Successfully scraped ' + seed)  # noqa: E501; prints a message so we know it worked
     
 def get_hrefs(filename):
-    sites = []
-    with open('scrape/aa.html') as aa_file:
-        soup = BeautifulSoup(aa_file, 'html.parser')
-    for link in soup.find_all('a'):
-        print('link type', type(link))
-        url = link.get('href')
-        end = url.split('/')[-1]
-        sites.append(end)
-    print(sites)
+    """ Opens the file passed to it and parses for the other links on the page """  #noqa: E501
+    sites = []  # creates an empty list to add the URLs to
+    with open('scrape/' + filename) as html_file:  # opens the file passed to it
+        soup = BeautifulSoup(html_file, 'html.parser')  # noqa: E501 creates variable soup that has the read content of the file via BS
+    for link in soup.find_all('a'):  # finds all the a elements on the page
+        print('link type', type(link))  # prints the type of all the a elements
+        url = link.get('href')  # gets the value for all the href keys
+        end = url.split('/')[-1]  # splits the value (a url) on its last slash
+        sites.append(end)  # adds the end of the url to the empty list
+    print(sites)  # prints the list to verify things worked correcrtly
 
 """    
     
