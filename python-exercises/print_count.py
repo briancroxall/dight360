f5037fa41ce0c6310db108e7d6bf56905fab9691
briancroxall <b.croxall@gmail.com>
2018-03-07 16:27:42 -0700

    Class execrise

--- python-exercises/print_count.py
+++ python-exercises/print_count.py
@@ -8,6 +8,8 @@ Created on Wed Mar  7 15:20:26 2018
 
 import sys
 
+print(sys.argv)
 for each in sys.argv[1:]:
     with open(each) as my_file:
         print(each, my_file.read().count('print('), sep='\t')
+        
